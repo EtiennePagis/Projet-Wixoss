e4df4adb312373e1bd65abedb5a8b0e5486f9416
victorien grude <spi2023@ic2s124-16.univ-lemans.fr>
2014-11-19 10:44:35 +0100

    ajout des level

--- classes.c
+++ classes.c
@@ -6,5 +6,5 @@ enum typedef(Orc = 1, Gobelin , Bete , Monstre , Insecte , Golem, Lezard) t_race
 enum typedef(Warrior = 1 , Archer , Mage , Invoker, Druid , Paladin , Singer, Summoner, Thief, Necromancian, Healer, Speed-fighter,Dragonborn)t_classe; 
 enum typedef(Feu = 1, Glace, Lumiere , Tenebre ,Soin )t_attribut;
 struct typedef (int con ; int Hp ; int force ; int intel ; int def ; int res; int agi;)t_stat; 
-struct typedef (char nom; t_race race ; t_classe classe ; t_attribut attribut ; t_stat stat;) t_personnage;
+struct typedef (char nom; t_race race ; t_classe classe ; t_attribut attribut ; t_stat stat; int level ;) t_personnage;
 struct typedef (t_race_ennemie race_ennemie ; t_classe classe ; t_attribut attribut ; t_stat stat;) t_ennemie;
