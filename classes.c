9b3480897ee9fe24a04bbde3fa50080e77b61911
victorien grude <spi2023@ic2s124-16.univ-lemans.fr>
2014-11-19 11:29:27 +0100

    Ajout de l'exp

--- classes.c
+++ classes.c
@@ -6,5 +6,5 @@ enum typedef(Orc = 1, Gobelin , Bete , Monstre , Insecte , Golem, Lezard) t_race
 enum typedef(Warrior = 1 , Archer , Thief , Soldier, Sorcer , Healer, Knight,Paladin,Gladiator,Destroyer,Sentinel,Elite,Marksman,Sniper,Assassin,Ninja,Rogue,Trickster,Lord,General,Brawler,Speed-fighter,White-Mage,Sage,Black-Mage,Warlock,Priest,Oracle,Monk,Cleric)t_classe; 
 enum typedef(Feu = 1, Glace, Lumiere , Tenebre ,Soin )t_attribut;
 struct typedef (int con ; int Hp ; int force ; int intel ; int def ; int res; int agi;)t_stat; 
-struct typedef (char nom; t_race race ; t_classe classe ; t_attribut attribut ; t_stat stat; int level ;) t_personnage;
+struct typedef (char nom; t_race race ; t_classe classe ; t_attribut attribut ; t_stat stat; int level ;int exp;) t_personnage;
 struct typedef (t_race_ennemie race_ennemie ; t_classe classe ; t_attribut attribut ; t_stat stat;) t_ennemie;
