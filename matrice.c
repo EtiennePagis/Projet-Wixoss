d6aec172a297a75e8d8748dc7ea8f5a1d215abc6
etienne pagis <spi2048@ic2s124-14.univ-lemans.fr>
2014-11-19 11:36:25 +0100

    Marche pas a revoir

---
+++ matrice.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 10
+#define M 20
+
+
+int main (){
+	char mat[N][M];
+	int i,j;
+	for(i=0 ; i< N;i++){
+		for(j=0; j < M; j++){
+			mat[i][j] = '_' ;
+		}
+	}
+	printf("|" );
+	for(i=0 ; i< N;i++){
+		
+		for(j = 0; j < M; j++){
+			if(i%2 == 1) {
+				printf("%c|",mat[i][j]) ;
+			}else {
+				printf("| ");
+			}
+			
+		}
+		if(i%2 == 1) printf("\n|");
+		if(i%2 == 0) printf("\n");
+	}
+}
