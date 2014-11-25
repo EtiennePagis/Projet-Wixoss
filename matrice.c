9551a675b30c474d3b2cb26282a17019b7e987b0
etienne pagis <spi2048@ic2s124-02.univ-lemans.fr>
2014-11-25 15:11:13 +0100

    La fonction matrice modifié afin de placer des entité

--- matrice.c
+++ matrice.c
@@ -1,30 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 10
-#define M 20
+#include <time.h>
+#define N 9
+#define M 7
+int mat[N][M];
 
-
-int main (){
-	char mat[N][M];
+void affichage (){
+	int i,j;
+	printf(" ");
+	for(i=0 ; i< N;i++){
+		for(j = 0; j < M; j++){
+			if(i!=0||j!=0){
+				printf("|%i",mat[i][j]) ;
+			}
+		}
+		printf("|\n");
+	}
+}
+void init(){
 	int i,j;
 	for(i=0 ; i< N;i++){
 		for(j=0; j < M; j++){
-			mat[i][j] = '_' ;
+			if (i==0){
+				mat[i][j] = j;
+			}else{
+				if(j==0){
+					mat[i][j] = i;
+				}else{
+					mat[i][j] = 0;
+				}
+			}
 		}
 	}
-	printf("|" );
-	for(i=0 ; i< N;i++){
-		
-		for(j = 0; j < M; j++){
-			if(i%2 == 1) {
-				printf("%c|",mat[i][j]) ;
-			}else {
-				printf("| ");
-			}
-			
-		}
-		if(i%2 == 1) printf("\n|");
-		if(i%2 == 0) printf("\n");
-	}// a marche pas
+	//mat[7][5] = 1;
+}
 
+void placement () 
+{
+	srand(time(NULL));
+	int pos_x , pos_y;
+	int player , ennemy;
+	printf(" Veuillez rentrez le nombre d'entité du joueur a placer : ");
+	scanf("%i",&player);
+	ennemy = rand()%(15 - 1)+1;
+	for(player; player > 0; player--) {
+		pos_x = rand()%(8-1)+1;
+		pos_y = rand()%(6-1)+1;
+		
+		while(mat[pos_x][pos_y] != 0) {
+			pos_x = rand()%(8-1)+1;
+			pos_y = rand()%(6-1)+1;
+		}
+		
+		mat[pos_x][pos_y] = 1;
+	}
+	for(ennemy; ennemy > 0; ennemy--) {
+		pos_x = rand()%(8-1)+1;
+		pos_y = rand()%(6-1)+1;
+		
+		while(mat[pos_x][pos_y] != 0) {
+			pos_x = rand()%(8-1)+1;
+			pos_y = rand()%(6-1)+1;
+		}
+		
+		mat[pos_x][pos_y] = 2;
+	}
+}
+	 
+	
+void main ()
+{
+	init();
+	placement();
+	affichage();
 }
