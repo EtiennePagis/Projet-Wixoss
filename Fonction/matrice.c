b1e8f63ae5129aa3237e8eb5bf75d3195378d06a
etienne pagis <spi2048@ic2s122-07.univ-lemans.fr>
2014-12-01 15:08:29 +0100

    Encore des modifs

--- Fonction/matrice.c
+++ Fonction/matrice.c
@@ -14,11 +14,7 @@ void affichage (){
 	for(i=0 ; i< N;i++){
 		for(j = 0; j < M; j++){
 			if(i!=0||j!=0){
-				if (mat[i][j]!=0){
-					printf("|%i",mat[i][j]) ;
-				}else{
-					printf("| ");
-				}
+				printf("|%i",mat[i][j]) ;
 			}
 		}
 		printf("|\n");
@@ -40,22 +36,24 @@ void init(){
 			}
 		}
 	}
-
+	//mat[7][5] = 1;
 }
 
 void placement () 
 { // Place les entité des joueurs et des ennemis 
-	
+	srand(time(NULL));
 	int pos_x , pos_y;
 	int player , ennemy;
+	int boss = 1;
 	printf("Veuillez rentrer le nombre d'entité du joueur a placer : \n");
 	scanf("%i",&player);
-	while((player <= 0)||(player >= 7)){
-		printf("Nombre d'entité du joueur invalide\n");
+	while((player <= 0)||(player >= 7))
+	{
+		printf("Nombre d'entité du joueur invalide\n"); // ça marche ça 
 		printf("Veuillez rentrer à nouveaux le nombre d'entité du joueur a placer : \n");
 		scanf("%i",&player);
 	}
-	ennemy = rand()%(14)+2;
+	ennemy = rand()%(15 - 1)+1;
 	for(player; player > 0; player--) {
 		pos_x = rand()%(8-1)+1;
 		pos_y = rand()%(6-1)+1;
@@ -77,6 +75,20 @@ void placement ()
 		}
 		
 		mat[pos_x][pos_y] = 2;
+	}
+	for(boss; boss > 0; boss--) 
+	{
+		pos_x = rand()%(8-1)+1;
+		pos_y = rand()%(6-1)+1;
+		while((mat[pos_x][pos_y] != 0)&&(pos_x == 8)&&(pos_x == 1)&&(pos_y == 1)&&(pos_y == 6)) 
+		{
+			pos_x = rand()%(8-1)+1;
+			pos_y = rand()%(6-1)+1;
+		}
+		mat[pos_x][pos_y] = 3;
+		mat[pos_x+1][pos_y+1] = 3;
+		mat[pos_x+1][pos_y] = 3;
+		mat[pos_x][pos_y+1] = 3;
 	}
 }
 
