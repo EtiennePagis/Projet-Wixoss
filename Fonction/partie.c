9895327f4d5562d5d0b6c5439c5ce1a9e596e4ea
victorien grude <spi2023@ic2s124-07.univ-lemans.fr>
2015-01-06 15:16:45 +0100

    documentation partie

--- Fonction/partie.c
+++ Fonction/partie.c
@@ -5,6 +5,19 @@
 #include "matrice.h"
 #include "deplacement_ennemi.h"
 
+
+/**
+*\file partie.c
+*\brief Projet 
+*\author Etienne Pagis Stevy Fouquet Victorien Grude
+*\version 0.1
+*\date 6 Janvier 2015
+*/
+
+/**
+*\fn partie (void) 
+*\brief lance les differente fonction utile au deroulement d'une partie
+*/
 int partie (void) 
 
 {
