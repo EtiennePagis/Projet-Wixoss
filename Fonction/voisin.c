f38758186218b99326bd6f9b520b260897740478
victorien grude <spi2023@ic2s124-07.univ-lemans.fr>
2015-01-06 14:59:12 +0100

    fonction attaque final (manque plus que l'algorithme de degat)

--- Fonction/voisin.c
+++ Fonction/voisin.c
@@ -11,11 +11,26 @@ int mat[N][M];
 int boss_mort = 0;
 int pile_ennemi[TMAX];
 int sommet_ennemi =-1;
+/**
+*\file voisin.c
+*\brief Projet 
+*\author Etienne Pagis Stevy Fouquet Victorien Grude
+*\version 0.1
+*\date 6 Janvier 2015
+*/
 
+/**
+*\fn initpile(int pile[],int* sommet)
+*\brief Initialise une pile mise en parametre
+*/
 void initpile(int pile[],int* sommet){
 	*sommet = -1;
 }
 
+/**
+*\fn empiler(int e,int pile[],int* sommet)
+*\brief Empile un entier mit en parametre sur une pile mise en parametre 
+*/
 void empiler(int e,int pile[],int* sommet){
 	if (*sommet < TMAX - 1){
 		*sommet = *sommet + 1;
@@ -23,6 +38,10 @@ void empiler(int e,int pile[],int* sommet){
 	}
 }
 
+/**
+*\fn depiler(int* e, int pile[], int* sommet)
+*\brief Depile un entier dans un pointeur mit en parametre sur une pile mise en parametre 
+*/
 void depiler(int* e, int pile[], int* sommet){
 	if(*sommet > -1){
 		*e = pile[*sommet];
@@ -30,12 +49,19 @@ void depiler(int* e, int pile[], int* sommet){
 	}	
 } 
 
+/**
+*\fn pilevide(int pile[],int sommet)
+*\brief Renvoie vrai si la pile mise en parametre est vide 
+*/
 int pilevide(int pile[],int sommet){
 	return(sommet == -1);
 }
 
 
-
+/**
+ *\fn est_ennemi(int i,int j,int joueur)
+ *\brief Renvoie vrai si la premiere unite mise en parametre est enemis a la deuxieme unite mise en parametre 
+ */
 int est_ennemi(int i,int j,int joueur){
 	if(joueur == 1){
         if (mat[i][j] == 2 || mat[i][j] == 3){return(1);}
@@ -49,18 +75,30 @@ int est_ennemi(int i,int j,int joueur){
     return(0);
 }
 
+/**
+ *\fn est_allie(int i, int j,int joueur)
+ *\brief Renvoie vrai si la premiere unite mise en parametre est allie a la deuxieme unite mise en parametre 
+ */
 int est_allie(int i, int j,int joueur){
 	if (joueur != 0){
 		if(est_ennemi(i,j,joueur)){
 			return(0);
-		}else{
+		}else if (mat[i][j] != 0 && mat[i][j] != 5){
 			return(1);
+		}else{
+			return(0);
 		}
 	}else{
 		return(-1);
 	}
 }
 
+
+
+/**
+*\fn est_tenaille_vertical(int i,int j)
+*\brief Renvoie vrai si l'unite mise en parametre est prise en tenaille verticalement
+*/
 int est_tenaille_vertical(int i,int j){
 	if(i != 1 && i != 8 ){ //on n'est pas sur un bord horizontale
 		while((i >= 2 )&& (!est_ennemi(i-1,j,mat[i][j])) && (mat[i-1][j] !=0)){
@@ -92,6 +130,10 @@ int est_tenaille_vertical(int i,int j){
 	return(0);
 }
 
+/**
+*\fn est_tenaille_horizontal(int i,int j)
+*\brief Renvoie vrai si l'unite mise en parametre est prise en tenaille horizontalement
+*/
 int est_tenaille_horizontal(int i,int j){
 	
 	if(j != 1 && j != 6 ){ //on n'est pas sur un bord vertical
@@ -125,6 +167,10 @@ int est_tenaille_horizontal(int i,int j){
 	
 }
 
+/**
+*\fn est_tenaille_coin(int i,int j)
+*\brief Renvoie vrai si l'unite mise en parametre est prise en tenaille alors qu'elle est dans un coin
+*/
 int est_tenaille_coin(int i,int j){
 	//pour le moment, dans les coins, on ne eput pas etre pris en tenaille
 		
@@ -132,8 +178,10 @@ int est_tenaille_coin(int i,int j){
 	
 }
 
-
-
+/**
+*\fn est_tenaille(int i,int j)
+*\brief Renvoie vrai si l'unite mise en parametre est prise en tenaille
+*/
 int est_tenaille(int i,int j){
 	if(est_tenaille_vertical(i,j)||est_tenaille_horizontal(i,j)||est_tenaille_coin(i,j)){
 		return(1);
@@ -142,7 +190,10 @@ int est_tenaille(int i,int j){
 }
 
 
-
+/**
+*\fn voisin(int i,int j)
+*\brief Renvoie le nombre de voisin de l'unite mise en parametre
+*/
 int voisin(int i,int j)
 {
 	int k,l;
@@ -159,12 +210,18 @@ int voisin(int i,int j)
     
     for(l = j;(l < M)&&(!est_ennemi(k,l,mat[i][j]));l++){
         if(est_allie(k,l,mat[i][j])){
-            voisin++;
+			if(!(k == i && l == j)){
+				printf("l'unité en %i %i est un voisin à l'unité %i %i\n",k,l,i,j);
+				voisin++;
+			}
         }
     }
     for(l = j;(l > 0)&&(!est_ennemi(k,l,mat[i][j]));l--){
         if(est_allie(k,l,mat[i][j])){
-            voisin++;
+			if(!(k == i && l == j)){
+				printf("l'unité en %i %i est un voisin à l'unité %i %i\n",k,l,i,j);
+				voisin++;
+			}
         }
     }
     
@@ -172,21 +229,35 @@ int voisin(int i,int j)
     
     for(k = i;(k < N)&&(!est_ennemi(k,l,mat[i][j]));k++){
         if(est_allie(k,l,mat[i][j])){
-            voisin++;
+			
+			if(!(k == i && l == j)){
+				printf("l'unité en %i %i est un voisin à l'unité %i %i\n",k,l,i,j);
+				voisin++;
+			}
         }
     }
     for(k = i;(k > 0)&&(!est_ennemi(k,l,mat[i][j]));k--){
         if(est_allie(k,l,mat[i][j])){
-            voisin++;
+			
+			if(!(k == i && l == j)){
+				printf("l'unité en %i %i est un voisin à l'unité %i %i\n",k,l,i,j);
+				voisin++;
+			}
         }
     }
-    if (mat[i][j] != 3){
+   /* if (mat[i][j] != 3){
 		return(voisin - 4); //return voisin -4 car dans chacune des boucles on compte la case mat[i][j] qui est le personnage
 	}else{
 		return(voisin); // ici il ne faut pas return voisin -4 car nous ne comptons pas la case cibler ni les autres cases du boss
-	}
+	}*/
+	return(voisin);
 }
 
+
+/**
+*\fn attaque_joueur(int i,int j)
+*\brief Fais l'attaque de l'unite joueur mis en parametre
+*/
 void attaque_joueur(int i,int j){
 	int ei,ej;		//ici les variables ei et ej servent à donner les coordonnées de l'ennemi
 	int nb_ennemis;
@@ -245,7 +316,70 @@ void attaque_joueur(int i,int j){
 	} 
 }
 
+/**
+*\fn attaque_ennemis(int i,int j)
+*\brief Fais l'attaque de l'unite ennemis mis en parametre
+*/
+void attaque_ennemis(int i,int j){
+	int ei,ej;		//ici les variables ei et ej servent à donner les coordonnées de l'ennemi
+	int nb_ennemis;
+    int pile_att[TMAX];
+    int voisins;
+    int sommet_att;
+	nb_ennemis = 0;
+	voisins = voisin(i,j);
+	initpile(pile_att,&sommet_att);
+	
+	if (i <= 7){
+		if (mat[i+1][j] == 1){
+			empiler(i+1,pile_att,&sommet_att);
+			empiler(j,pile_att,&sommet_att);
+			nb_ennemis++;
+		}
+	}
+	if (i >= 2){
+		if (mat[i-1][j] == 1){
+			empiler(i-1,pile_att,&sommet_att);
+			empiler(j,pile_att,&sommet_att);
+			nb_ennemis++;
+		}
+	}
+	if (j <= 5){
+		if (mat[i][j+1] == 1){
+			empiler(i,pile_att,&sommet_att);
+			empiler(j+1, pile_att,&sommet_att);
+			nb_ennemis++;
+		}
+	}
+	if (j >= 2){
+		if (mat[i][j-1] == 1){
+			empiler(i,pile_att,&sommet_att);
+			empiler(j-1,pile_att,&sommet_att);
+			nb_ennemis++;
+		}
+	}
+	while(nb_ennemis > 0){
+		depiler(&ej,pile_att,&sommet_att);
+		depiler(&ei,pile_att,&sommet_att);
+		if (est_tenaille(ei,ej)){
+			// les degats ne sont actuelement pas calculer ici l'uniter toucher est obligatoirement morte
+			while(!pilevide(pile_ennemi,sommet_ennemi)){
+				depiler(&ej,pile_ennemi,&sommet_ennemi);
+				depiler(&ei,pile_ennemi,&sommet_ennemi);
+				mat[ei][ej] = 5;		
+				printf("L'ennemis en %i %i est mort car l'unité est prise en tenaille\n",ei,ej);
+			}
+		}else if(voisins > 0){
+			if (mat[ei][ej] == 1){
+				mat[ei][ej] = 5;		
+				printf("L'ennemis en %i %i est mort car l'ennemis en %i %i qui attaque a %i voisins\n",ei,ej,i,j,voisins);
+			}
+		}
+		nb_ennemis--;
+	} 
+}
 
+/*
 int main(){
 	srand(time(NULL));
 	// initialisation de la matrice a 0
@@ -264,18 +398,18 @@ int main(){
 		}
 	}
 	
-	mat[8][1] = 1;
+	mat[8][1] = 0;
 	mat[8][3] = 2;
 	mat[8][4] = 2;
 	mat[8][5] = 1;
-	mat[6][2] = 0;
+	mat[8][6] = 0;
 	mat[7][2] = 2;
 	mat[8][2] = 2;
     mat[7][1] = 2;
     mat[6][1] = 2;
     mat[5][1] = 2;
     mat[4][1] = 1;
-
+	
 	int pos_x , pos_y;
 	int player , ennemy;
 	int boss = 1;
@@ -334,18 +468,11 @@ int main(){
 		printf("|\n");
 	}
 	
-	/*int voisins;
-	printf("Donnez les coordonnées de l'unite dont vous voulez le nombre de voisin : ");
-	scanf("%i%i",&i,&j);
-	voisins = voisin(i,j);
-	if (voisins != -1) {
-		printf("Cette unite a %i voisins \n",voisins);
-	}
-	*/
+	
 	for(i=1 ; i< N;i++){
 		for(j = 1; j < M; j++){
 			if(mat[i][j] == 1){
-				printf(" ===================== attaque du perso en %i %i (%i) pour l'équipe 1 ===================== \n", i, j ,mat[i][j]);
+				printf("\n\n ===================== attaque du perso en %i %i (%i) pour l'équipe Joueur ===================== \n\n", i, j ,mat[i][j]);
 				attaque_joueur(i,j);
 			}
 		}
@@ -360,7 +487,26 @@ int main(){
 			}
 		}
 	}
-	printf("  ");
+	for(i=1 ; i< N;i++){
+		for(j = 1; j < M; j++){
+			if(mat[i][j] == 2||mat[i][j] == 3){
+				printf("\n\n ===================== attaque du perso en %i %i (%i) pour l'équipe Bot ===================== \n\n", i, j ,mat[i][j]);
+				attaque_ennemis(i,j);
+			}
+		}
+	}
+	for(i=1 ; i< N;i++){
+		for(j = 1; j < M; j++){
+			if(mat[i][j] == 5){
+				mat[i][j] = 0;
+			}
+			if (boss_mort&&mat[i][j] == 3){
+				mat[i][j] = 0;
+			}
+		}
+	}
+	
+	printf("\n\n  ");
 	for(i=0 ; i< N;i++){
 		for(j = 0; j < M; j++){
 			if(i!=0||j!=0){
@@ -376,7 +522,6 @@ int main(){
 	}
 	return(EXIT_SUCCESS);
 }
-
-
+*/
 
 
--- Fonction/voisin.h
+++ Fonction/voisin.h
@@ -4,4 +4,21 @@
 #ifndef VOISIN_H
 #define VOISIN_H
 #endif
-int voisin(int i, int j);
+
+void initpile(int pile[],int* sommet);
+void empiler(int e,int pile[],int* sommet);
+void depiler(int* e, int pile[], int* sommet);
+int pilevide(int pile[],int sommet);
+
+int est_ennemi(int i,int j,int joueur);
+int est_allie(int i, int j,int joueur);
+
+int est_tenaille_vertical(int i,int j);
+int est_tenaille_horizontal(int i,int j);
+int est_tenaille_coin(int i,int j);
+int est_tenaille(int i,int j);
+
+int voisin(int i,int j);
+
+void attaque_joueur(int i,int j);
+void attaque_ennemis(int i,int j);
