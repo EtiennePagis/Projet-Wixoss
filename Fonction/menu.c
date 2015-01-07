c1827c4a4a845316e2e75fb3802a05ad3dce9795
victorien grude <spi2023@ic2s124-16.univ-lemans.fr>
2015-01-07 17:08:39 +0100

    menu end

--- Fonction/menu.c
+++ Fonction/menu.c
@@ -25,7 +25,8 @@ int mat[N][M];
 int main () 
 {
 	srand(time(NULL));
-	int choix; 
+	int choix = 0; 
+	int niveau = 1;
 	system("clear");
 	
 	do
@@ -35,16 +36,18 @@ int main ()
 		//printf("1 - To create or choose your character \n");
 		printf("1 - Launch game \n");
 		printf("2 - See your squad or modif them \n");
-		printf("3 - Quit  \n");
+		printf("3 - Change the difficulty \n");
+		printf("4 - Quit  \n");
 		printf("What is your choice  ? \n");
 		scanf("%d",&choix); 
 	
 		switch(choix) 
 		{
-			case 1 : system("clear"); partie(); break;
+			case 1 : system("clear");if(escouade1.nb_perso >= 2){ partie();}else{printf("Vous devez avoir au moins 2 personnage dans votre escouade\n");}; break;
 			case 2 : system("clear"); menu_personnage(); break;
-			case 3 : break;
-			default : printf("Your choice is undefined , your must put a number between 1 to 2 \n");
+			case 3 : system("clear");printf("Veuillez choisir la difficulté :\n1 : Tres facil \n2 : Facil \n3 : Moyen \n4 : Difficil \n5 : Tres Difficil");scanf("%i",&niveau);break;
+			case 4 : break;
+			default : printf("Your choice is undefined , your must put a number between 1 to 4 \n");
 		
 		}
 	}
